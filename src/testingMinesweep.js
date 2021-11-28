fda77fd542c06b8f60c3f6288c6dedd530bc630c
Fruzsina Strbik <fruzsina.strbik1@ibm.com>
2021-11-28 19:32:36 +0100

    GREEN

--- src/testingMinesweep.js
+++ src/testingMinesweep.js
@@ -1,2 +1,6 @@
-const testingMinesweep = () => {};
-module.exports = { testingMinesweep };
+const createBoard = () => {
+  const board = '+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+';
+  console.log(board);
+  return board;
+};
+module.exports = { createBoard };
--- src/testingMinesweep.test.js
+++ src/testingMinesweep.test.js
@@ -1,7 +1,7 @@
 const { createBoard } = require('./testingMinesweep');
 
 describe('Board creation', () => {
-  it('Given the board is 3x3 When I start the game Then should return "+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+"', () => {
+  it('Given the board is 3x3 When I start the game Then should return 3x3 empty board', () => {
     expect(createBoard()).toBe(
       '+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+'
     );
