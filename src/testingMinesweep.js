f2c69f92814df8e54b909cb28638946025e19f51
Fruzsina Strbik <fruzsina.strbik1@ibm.com>
2021-11-29 20:59:55 +0100

    RED

--- src/testingMinesweep.js
+++ src/testingMinesweep.js
@@ -1,3 +1,6 @@
+const BOMB = 'X';
+const FLAG = '*';
+
 class MineSweeper {
   board = '+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+';
 
@@ -16,41 +19,39 @@ class MineSweeper {
     for (let i = 0; i < mineNumber; i++) {
       const posX = Math.floor(Math.random() * 3);
       const posY = Math.floor(Math.random() * 3);
-      this.matrix[posX][posY] = 'X';
+      this.matrix[posX][posY] = BOMB;
     }
     return this.matrix;
   };
 
   clickCell = (x, y) => {
-    if (this.matrix[x][y] !== 'X') {
-      const position = this.sBoard.split(' ', 3 * x + y + 1).join(' ').length;
+    if (this.matrix[x][y] !== BOMB) {
       let mineCount = 0;
       for (let i = Math.max(x - 1, 0); i <= Math.min(x + 1, 2); i++) {
         for (let j = Math.max(y - 1, 0); j <= Math.min(y + 1, 2); j++) {
-          if (this.matrix[i][j] === 'X') {
+          if (this.matrix[i][j] === BOMB) {
             mineCount++;
           }
         }
       }
-      this.board = `${this.board.substr(
-        0,
-        position
-      )}${mineCount}${this.board.substr(position + 1)}`;
+      this.revealCell(x, y, mineCount);
       return 'Cleared cell';
     }
-    const position = this.sBoard.split(' ', 3 * x + y + 1).join(' ').length;
-    this.board = `${this.board.substr(0, position)}X${this.board.substr(
-      position + 1
-    )}`;
+    this.revealCell(x, y, BOMB);
     return 'Game Over';
   };
 
   flagCell = (x, y) => {
-    const position = this.sBoard.split(' ', 3 * x + y + 1).join(' ').length;
-    this.board = `${this.board.substr(0, position)}*${this.board.substr(
-      position + 1
-    )}`;
+    this.revealCell(x, y, FLAG);
     return 'Cell flagged as bomb';
+  };
+
+  revealCell = (x, y, character) => {
+    const position = this.sBoard.split(' ', 3 * x + y + 1).join(' ').length;
+    this.board = `${this.board.substr(
+      0,
+      position
+    )}${character}${this.board.substr(position + 1)}`;
   };
 }
 
--- src/testingMinesweep.test.js
+++ src/testingMinesweep.test.js
@@ -133,4 +133,15 @@ describe('Testing the Mine Sweeper', () => {
       );
     });
   });
+
+  describe('Clicking on a 0', () => {
+    it('Given a board with bomb on [0,0] When I click on [2,2] Then cells next to it will open', () => {
+      global.Math.random = () => 0;
+      game.addMines();
+      game.clickCell(2, 2);
+      expect(game.board).toBe(
+        '+-+-+-+\n| |1|_|\n+-+-+-+\n|1|1|_|\n+-+-+-+\n|_|_|_|\n+-+-+-+'
+      );
+    });
+  });
 });
